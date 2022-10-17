68539698fccc112c1e8a01ce0a5da8f7d2d35a40
rafabue <xiaomirafaelbueno@gmail.com>
2022-10-17 13:32:37 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+        int a = 5;
+        int b = 3;
+        System.out.println("Teste primeiro programa! " + (a+b));
+    }
+}
+
+// ALT + INSERT: Atalho para criar classes, pa cotes e etc dentro do projeto.
+// CTRL + Y: Atalho para apagar linhas.
+// CTRL + D: Atalho para duplicar linhas.
+// CTRL + SHIFT + SETA: Atalho para mover uma linha de posição.
